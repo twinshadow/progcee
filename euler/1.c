1736bb6152f3dd95ef126a14efd3e097a01abc79
Anthony Cornehl <accornehl@gmail.com>
2012-12-08 20:15:16 -0800

    indentation and formatting

--- euler/1.c
+++ euler/1.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
-int main(void)
+int
+main(void)
 {
-  int i = 1,
-      limit = 1000,
-      sum = 0;
+	int i = 1, limit = 1000, sum = 0;
 
-  while (i < limit) {
-    if (i % 3 == 0 || i % 5 == 0) {
-      sum += i;
-    }
+	while(i < limit)
+	{
+		if(i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
 
-    i += 1;
-  }
+		i += 1;
+	}
 
-  printf("%i is the sum of all multiples of 3 or 5 under %i\n", sum, limit);
-  return 0;
+	printf("%i is the sum of all multiples of 3 or 5 under %i\n", sum,
+	       limit);
+	return 0;
 }
--- euler/2.c
+++ euler/2.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
-int main(void)
+int
+main(void)
 {
-  unsigned long int i = 1,
-                    prev = 0,
-                    hold = 0,
-                    limit = 4000000,
-                    sum = 0;
+	unsigned long int i = 1, prev = 0, hold = 0, limit = 4000000, sum = 0;
 
-  while (i < limit) {
-    if ((i & 1) == 0) {
-      sum += i;
-    }
+	while(i < limit)
+	{
+		if((i & 1) == 0)
+		{
+			sum += i;
+		}
 
-    hold = i + prev;
-    prev = i;
-    i = hold;
-  }
+		hold = i + prev;
+		prev = i;
+		i = hold;
+	}
 
-  printf("%ld is the sum of all even fibonacci numbers under %ld\n", sum, limit);
-  return 0;
+	printf("%ld is the sum of all even fibonacci numbers under %ld\n", sum,
+	       limit);
+	return 0;
 }
