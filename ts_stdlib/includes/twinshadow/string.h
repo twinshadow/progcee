585881d29dba7a1bec78fba0f55b41df0e87c863
Phillip Garland <pgarland@gmail.com>
2013-05-02 20:43:33 -0700

    strshift: Make work for arrays of arbitrary type

--- ts_stdlib/includes/twinshadow/string.h
+++ ts_stdlib/includes/twinshadow/string.h
@@ -4,6 +4,6 @@
 char *strrtok_r(char *str, const char delim, char *lastptr);
 char *strrtok(char *str, const char delim);
 void strstrip(char *str, const int count);
-void strshift(int offset, char *str, const int count);
+void strshift(int offset, void *str, const int count, size_t size);
 
 #endif /* TWINSHADOW_STRING_H */
