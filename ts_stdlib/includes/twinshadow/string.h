bcc6440af0f865660e29b63f1aa1faeed4f41e17
Anthony Cornehl <accornehl@gmail.com>
2013-06-24 18:18:32 -0700

    Add strdup functions, remove defunct test folder

--- ts_stdlib/includes/twinshadow/string.h
+++ ts_stdlib/includes/twinshadow/string.h
@@ -5,5 +5,7 @@ char *strrtok_r(char *str, const char delim, char *lastptr);
 char *strrtok(char *str, const char delim);
 void strstrip(char *str, const int count);
 void strshift(int offset, char *str, const int count);
+char *ts_strndup(const char *src, size_t len);
+char *ts_strdup(const char *src);
 
 #endif /* TWINSHADOW_STRING_H */
