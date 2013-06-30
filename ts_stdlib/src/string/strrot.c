e23cf1ade8a3f39469f6f7e5eb98caf2c33c5f75
Anthony Cornehl <accornehl@gmail.com>
2013-06-30 14:58:56 -0700

    ts_stdlib: strrot code complete with coverage test
    Added some additional make-target changes, refining check-run and adding
    analyze for clang

--- ts_stdlib/src/string/strrot.c
+++ ts_stdlib/src/string/strrot.c
@@ -3,11 +3,45 @@
 #include "twinshadow/twinshadow.h"
 
 void
-strnrot(int offset, char *str, size_t len)
+ts_strnrot(int offset, char *str, size_t len)
 {
-	static char *lower = "abcdefghijklmnopqrstuvwxyz";
-	static char *upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	static char *digit = "0123456789";
+	const char *ciphers[] = {
+		"abcdefghijklmnopqrstuvwxyz",
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		"0123456789",
+	};
+	const size_t ciphers_len = LENGTH(ciphers);
+	size_t cipher_strlen;
+	size_t cipher_idx;
+	char *chr_idx;
+	size_t idx;
 
-	REDUCE_OFFSET(offset, len);
+
+	for (idx = 0; idx < len; idx++)
+	{
+		for (cipher_idx = 0; cipher_idx < ciphers_len; cipher_idx++)
+		{
+			UNLESS ((chr_idx = strchr(ciphers[cipher_idx], str[idx])))
+				continue;
+
+			cipher_strlen = strlen(ciphers[cipher_idx]);
+			REDUCE_OFFSET(offset, cipher_strlen);
+			if (offset == 0 || offset == cipher_strlen)
+				continue;
+
+			str[idx] = (((chr_idx - ciphers[cipher_idx]) + offset
+						< cipher_strlen)
+					? *(chr_idx + offset)
+					: *(ciphers[cipher_idx] +
+						(((chr_idx - ciphers[cipher_idx]) + offset) - cipher_strlen)
+					)
+			);
+		}
+	}
+}
+
+void
+ts_strrot(int offset, char *str)
+{
+	ts_strnrot(offset, str, strnlen(str, SIZE_MAX));
 }
