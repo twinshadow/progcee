f08105d6d63d0315ce435df70ef8a6df1475523b
Anthony Cornehl <accornehl@gmail.com>
2013-07-04 22:37:21 +0000

    ts_stdlib: change vec cursor and sentinal to use pointers

--- ts_stdlib/includes/twinshadow/vec.h
+++ ts_stdlib/includes/twinshadow/vec.h
@@ -11,21 +11,21 @@
 /* Twinshadow vector meta-typing */
 #define TS_VEC_HEAD(__name, __type)		\
 	struct __name {				\
-		size_t sentinal;		\
-		size_t cursor;			\
+		__type *sentinal;		\
+		__type *cursor;			\
 		__type *vec;			\
 	}
 
 /* iterators */
-#define TS_VEC_FOREACH(__var, __head)				\
-	for (__var = __head->vec;				\
-		__var <= &__head->vec[__head->sentinal];	\
-		__var++)
+#define TS_VEC_FOREACH(__var, __head)	\
+	for (__var = __head->vec;	\
+	     __var <= __head->sentinal;	\
+	     __var++)
 
-#define TS_VEC_RFOREACH(__var, __head)				\
-	for (__var = &__head->vec[__head->sentinal];		\
-		__var >= __head->vec;				\
-		__var--)
+#define TS_VEC_RFOREACH(__var, __head)	\
+	for (__var = __head->sentinal;	\
+	     __var >= __head->vec;	\
+	     __var--)
 
 /* Memory functions */
 #define TS_VEC_NEW(__name, __type)				\
@@ -43,7 +43,7 @@
 			errno = ENOMEM;				\
 			return NULL;				\
 		}						\
-		ptr->sentinal = count - 1;			\
+		ptr->sentinal = &ptr->vec[count - 1];		\
 		ptr->cursor = 0;				\
 		return ptr;					\
 	}
@@ -51,6 +51,8 @@
 #define TS_VEC_FREE(__name)					\
 	void							\
 	__name##_free(struct __name *ptr){			\
+		if (ptr == NULL)				\
+			return;					\
 		free(ptr->vec);					\
 		free(ptr);					\
 		ptr = NULL;					\
@@ -59,25 +61,29 @@
 #define TS_VEC_RESIZE(__name, __type)					\
 	struct __name *							\
 	__name##_resize(struct __name *ptr, size_t count){		\
-		size_t old_sentinal;					\
+		size_t old_count, old_cursor;				\
 		if (ptr == NULL) {					\
 			errno = EACCES;					\
 			return NULL;					\
 		}							\
+		old_count = (ptr->sentinal - ptr->vec) / sizeof(__type);\
+		old_cursor = (ptr->cursor - ptr->vec) / sizeof(__type);	\
 		ptr->vec = realloc(ptr->vec, count * sizeof(__type));	\
 		if (ptr->vec == NULL) {					\
 			errno = ENOMEM;					\
 			return NULL;					\
 		}							\
-		old_sentinal = ptr->sentinal;				\
-		ptr->sentinal = count - 1;				\
-		if (old_sentinal < ptr->sentinal) {			\
-			memset(&ptr->vec[old_sentinal + 1],		\
-			    '\0', (ptr->sentinal - old_sentinal)	\
+		ptr->sentinal = &ptr->vec[count - 1];			\
+		if (old_count < count) {				\
+			memset(&ptr->vec[old_count],			\
+			    '\0', (count - old_count)			\
 			    * sizeof(__type));				\
 		}							\
-		if (ptr->cursor >= ptr->sentinal) {			\
+		if (old_cursor > count) {				\
 			ptr->cursor = ptr->sentinal;			\
+		}							\
+		else {							\
+			ptr->cursor = &ptr->vec[old_cursor];		\
 		}							\
 		return ptr;						\
 	}
