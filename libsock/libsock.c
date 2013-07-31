71b98c86b2d7a6e169110ac987cacbde38f4ad7a
Anthony Cornehl <accornehl@gmail.com>
2013-07-31 02:20:22 +0000

    Remove redundant includes

--- libsock/libsock.c
+++ libsock/libsock.c
@@ -1,10 +1,5 @@
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
-#include <errno.h>
 #include <netdb.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include "libsock.h"
